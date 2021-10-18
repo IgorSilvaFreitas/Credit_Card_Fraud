624f393de9a4f08087e032a52cb0691e02209e1d
Igor Freitas <igorfsouza18@gmail.com>
2021-10-18 20:23:52 -0300

    Update Unsupervised.R

--- Unsupervised.R
+++ Unsupervised.R
@@ -2,11 +2,11 @@
 base <- readr::read_csv("creditcard.csv")
 
 
-##Time variable should not interfire in the model, so it will be deleted
+##Time variable should not interfere in the model, so it will be deleted
 base <- base[,-1]
 
 
-## graph and modifying transaction variable to be similar to others
+## graph and modifying of Amount variable to be similar to others
 g1 <- base |>
   ggplot2::ggplot(ggplot2::aes(x=factor(Class, labels = c("No","Yes")),
                       y=Amount))+
@@ -21,3 +21,35 @@ plotly::ggplotly(g1)
 
 preproc <- caret::preProcess(base[,29], method=c("center", "scale","YeoJohnson"))
 base <- predict(preproc, base)
+
+
+
+## splitting sample
+train <- caret::createDataPartition(base$Class, p=0.75, list = F)
+training <- base[train,]
+testing <- base[-train,]
+
+
+##Scale
+training <- scale(training)
+testing <- scale(testing)
+
+##best cluster quantity
+factoextra::fviz_nbclust(training, kmeans, method="gap_stat")
+
+
+##Generating kmeans classifier
+base_kmeans <- kmeans(training, 4)
+
+
+##Graph to visualization
+factoextra::fviz_cluster(base_kmeans, data= training)
+
+
+##List with clusters
+list <- as.data.frame(base_kmeans$cluster)
+
+
+
+##Verifying accuracy
+factoextra::fviz_cluster(base_kmeans, data= testing)
